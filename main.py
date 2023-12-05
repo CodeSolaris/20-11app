b2619d5ba36edb673a5029f7539a26b5ca5cdc29
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-12-05 17:21:10 +0200

    * feat(main.py): add support for loading credit card data from cards.csv file * feat(main.py): create CreditCard class with number, expiry, holder, and cvc attributes * feat(main.py): add validate method to CreditCard class for validating credit card details * feat(main.py): create SecureCreditCard class as a subclass of CreditCard with additional pin attribute * feat(main.py): modify main interaction with the user to include credit card validation before booking hotel

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def load_hotels_dataframe() -> pd.DataFrame:
         return pd.read_csv('hotels.csv', dtype={'id': str})
     except FileNotFoundError:
         raise Exception("The hotels.csv file does not exist.")
+    
+#load credit cards data
+def load_cards_data() -> pd.DataFrame:
+    try:
+        return pd.read_csv('cards.csv', dtype=str).to_dict(orient = 'records')
+    except FileNotFoundError:
+        raise Exception("The cards.csv file does not exist.")
 
 # Save the DataFrame to the CSV
 def save_hotels_dataframe(df: pd.DataFrame) -> None:
@@ -14,7 +21,7 @@ def save_hotels_dataframe(df: pd.DataFrame) -> None:
 
 # Initialize the DataFrame
 df = load_hotels_dataframe()
-
+df_cards = load_cards_data()
 class Hotel:
     def __init__(self, hotel_ID: str):
         """
@@ -72,21 +79,73 @@ class ReservationTicket:
         """
         return ticket.strip()
 
-# Main interaction with the user
+
+class CreditCard:
+    def __init__(self, number: str, expiry: str, holder: str, cvc: str):
+ 
+        self.number = number
+        self.expiry = expiry
+        self.holder = holder.upper()
+        self.cvc = cvc
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the credit card details.
+        """
+        return f"""
+        Number: {self.number}
+        Expiry: {self.expiry}
+        Holder: {self.holder}
+        CVC: {self.cvc}
+        """
+    def validate(self) -> bool:
+        """
+        Validates the credit card details.
+        """
+        data_card = {
+            'number': self.number,
+            'expiration': self.expiry,
+            'cvc': self.cvc,
+            'holder': self.holder
+            
+        }
+        print (data_card)
+        if data_card in df_cards:
+            return True
+
+
+
+class SecureCreditCard(CreditCard):
+    def __init__(self, number: str, expiry: str, holder: str, cvv: str, pin: str):
+        super().__init__(number, expiry, holder, cvv)
+        self.pin = pin
+    def validate(self) -> bool:
+        """
+        Validates the credit card details.
+        """
+        return True
+
+#Main interaction with the user
 try:
     print(df)
     hotel_id = input('Enter the id of the hotel: ')
     hotel = Hotel(hotel_id)
-
     if hotel.name is None:
         print("Hotel with the given ID does not exist.")
-    else:
+
+    if hotel.is_available():
+        credit_card = CreditCard(number="1234567890123456", expiry="12/26", 
+                                    holder="John Smith", cvc="123")
+        
+        if not credit_card.validate():
+            print("Invalid credit card details.")
+            exit()
         name = input('Enter your name: ')
-        if hotel.is_available():
-            hotel.book_hotel()
-            reservation_ticket = ReservationTicket(name, hotel)
-            print(reservation_ticket.generate_ticket())
-        else:
-            print('Sorry, the hotel is not available.')
+        hotel.book_hotel()
+        reservation_ticket = ReservationTicket(name, hotel)
+        print(reservation_ticket.generate_ticket())
+    else:
+        print('Sorry, the hotel is not available.')
+
 except Exception as e:
     print(f"An error occurred: {e}")
