6e0f259360a86ba43f2d45358ba0a85bf3c1f62f
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-12-05 17:43:14 +0200

    * fix(card-security.csv): change column name from 'number' to 'card_number' * feat(main.py): load card security data from card-security.csv * feat(main.py): add authentication to SecureCreditCard class * feat(main.py): prompt user for password and authenticate credit card

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import pandas as pd
 from typing import Optional
 
+
 # Load the initial DataFrame
 def load_hotels_dataframe() -> pd.DataFrame:
     try:
-        return pd.read_csv('hotels.csv', dtype={'id': str})
+        return pd.read_csv("hotels.csv", dtype={"id": str})
     except FileNotFoundError:
         raise Exception("The hotels.csv file does not exist.")
-    
-#load credit cards data
+
+
+# load credit cards data
 def load_cards_data() -> pd.DataFrame:
     try:
-        return pd.read_csv('cards.csv', dtype=str).to_dict(orient = 'records')
+        return pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
     except FileNotFoundError:
         raise Exception("The cards.csv file does not exist.")
 
+
 # Save the DataFrame to the CSV
 def save_hotels_dataframe(df: pd.DataFrame) -> None:
-    df.to_csv('hotels.csv', index=False)
+    df.to_csv("hotels.csv", index=False)
+
 
 # Initialize the DataFrame
 df = load_hotels_dataframe()
 df_cards = load_cards_data()
+df_cards_security = pd.read_csv("card-security.csv", dtype=str)
+
+
 class Hotel:
     def __init__(self, hotel_ID: str):
         """
@@ -34,17 +41,17 @@ class Hotel:
         """
         Retrieves the name of the hotel based on the hotel_ID.
         """
-        hotel_info = df.loc[df['id'] == self.hotel_ID]
+        hotel_info = df.loc[df["id"] == self.hotel_ID]
         if not hotel_info.empty:
-            return hotel_info['name'].squeeze()
+            return hotel_info["name"].squeeze()
         return None
 
     def is_available(self) -> bool:
         """
         Checks if the hotel is available for booking.
         """
-        availability = df.loc[df['id'] == self.hotel_ID, 'available'].squeeze()
-        return str(availability).lower() == 'yes'
+        availability = df.loc[df["id"] == self.hotel_ID, "available"].squeeze()
+        return str(availability).lower() == "yes"
 
     def book_hotel(self) -> None:
         """
@@ -54,8 +61,9 @@ class Hotel:
             print("Hotel is not available for booking.")
             return
 
-        df.loc[df['id'] == self.hotel_ID, 'available'] = 'no'
+        df.loc[df["id"] == self.hotel_ID, "available"] = "no"
         save_hotels_dataframe(df)
+
 
 class ReservationTicket:
     def __init__(self, customer_name: str, hotel_object: Hotel):
@@ -82,7 +90,6 @@ class ReservationTicket:
 
 class CreditCard:
     def __init__(self, number: str, expiry: str, holder: str, cvc: str):
- 
         self.number = number
         self.expiry = expiry
         self.holder = holder.upper()
@@ -98,54 +105,60 @@ class CreditCard:
         Holder: {self.holder}
         CVC: {self.cvc}
         """
+
     def validate(self) -> bool:
         """
         Validates the credit card details.
         """
         data_card = {
-            'number': self.number,
-            'expiration': self.expiry,
-            'cvc': self.cvc,
-            'holder': self.holder
-            
+            "number": self.number,
+            "expiration": self.expiry,
+            "cvc": self.cvc,
+            "holder": self.holder,
         }
-        print (data_card)
+
         if data_card in df_cards:
             return True
 
 
-
 class SecureCreditCard(CreditCard):
-    def __init__(self, number: str, expiry: str, holder: str, cvv: str, pin: str):
-        super().__init__(number, expiry, holder, cvv)
-        self.pin = pin
-    def validate(self) -> bool:
-        """
-        Validates the credit card details.
-        """
-        return True
+    def authenticate(self, given_password) -> bool:
+        password = df_cards_security.loc[
+            df_cards_security["card_number"] == self.number, "password"
+        ].squeeze()
+        if password == given_password:
+            return True
 
-#Main interaction with the user
+
+# Main interaction with the user
 try:
     print(df)
-    hotel_id = input('Enter the id of the hotel: ')
+    hotel_id = input("Enter the id of the hotel: ")
     hotel = Hotel(hotel_id)
     if hotel.name is None:
         print("Hotel with the given ID does not exist.")
 
     if hotel.is_available():
-        credit_card = CreditCard(number="1234567890123456", expiry="12/26", 
-                                    holder="John Smith", cvc="123")
-        
+        credit_card = SecureCreditCard(
+            number="1234567890123456", expiry="12/26", holder="John Smith", cvc="123"
+        )
+
         if not credit_card.validate():
             print("Invalid credit card details.")
             exit()
-        name = input('Enter your name: ')
+
+        password = input("Enter your password: ")
+
+        if not credit_card.authenticate(password):
+            print("Invalid password.")
+            exit()
+
+        name = input("Enter your name: ")
         hotel.book_hotel()
         reservation_ticket = ReservationTicket(name, hotel)
         print(reservation_ticket.generate_ticket())
     else:
-        print('Sorry, the hotel is not available.')
+        print("Sorry, the hotel is not available.")
 
 except Exception as e:
     print(f"An error occurred: {e}")
